d11531745e7946833a6ca204aa35029879ba830f
Cr1sspY <sentluiz14@gmail.com>
2021-01-15 14:22:39 +0300

    Worked w VS

--- SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -5,10 +5,17 @@ namespace SpeakerMeet.API.Tests
 {
 public class SpeakerControllerSearchTests //<-- to rename
 {
-[Fact]
-public void ItExists()
-{
-var controller = new SpeakerController();
-}
-}
+    [Fact(Skip = "Более не требуется")]
+    public void ItExists()
+        {
+            var controller = new SpeakerController();
+        }
+
+    [Fact]
+    public void ItHasSearch() //Проверяем наличие метода Поиск
+        {
+            var controller = new SpeakerController();
+            controller.Search("Jos");
+        }
+    }
 }
