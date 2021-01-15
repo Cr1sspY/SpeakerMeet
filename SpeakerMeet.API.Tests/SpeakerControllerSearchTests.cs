5cba7980ae66ad58a2d82973d0f95f85e4d77cba
Cr1sspY <sentluiz14@gmail.com>
2021-01-15 14:40:29 +0300

    second commit

--- SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -1,21 +1,32 @@
 using System;
 using Xunit;
 using SpeakerMeet.API.Controllers; //<-- directive to add
+using Microsoft.AspNetCore.Mvc;
+
 namespace SpeakerMeet.API.Tests
 {
-public class SpeakerControllerSearchTests //<-- to rename
-{
-    [Fact(Skip = "Более не требуется")]
-    public void ItExists()
+    public class SpeakerControllerSearchTests //<-- to rename
+    {
+        [Fact(Skip = "Более не требуется")]
+        public void ItExists()
         {
             var controller = new SpeakerController();
         }
 
-    [Fact]
-    public void ItHasSearch() //Проверяем наличие метода Поиск
+        [Fact]
+        public void ItHasSearch() //Проверяем наличие метода Поиск
         {
             var controller = new SpeakerController();
             controller.Search("Jos");
+        }
+
+        [Fact]
+        public void ItReturnsOkObjectResult()
+        { 
+            var controller = new SpeakerController();
+            var result = controller.Search("Jos");
+            Assert.NotNull(result);                    //Поиск принес результат?
+            Assert.IsType<OkObjectResult>(result);     //Результат ОК?
         }
     }
 }
